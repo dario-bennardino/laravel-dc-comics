ba2c5a1a2f2816f1354519ba2c9ab16f46ca6301
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-05-17 17:15:45 +0200

    [add] inizio destroy

--- resources/views/Comics/show.blade.php
+++ resources/views/Comics/show.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <div class="container my-4 ">
-        <h1 class="text-center p-4 ">{{ $comic->title }}</h1>
+        <h1 class="text-center p-4 ">{{ $comic->title }} <a class="btn btn-warning"
+                href="{{ route('comics.edit', $comic) }} "><i class="fa-solid fa-pencil"></i></a> </h1>
         <div class="row row-cols-1 d-flex justify-content-center ">
             <div class="card mt-4" style="width: 25rem;">
                 <div class="col">

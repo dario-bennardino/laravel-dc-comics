4c489bea62ae316f2bd22a0e12a66f75ed8e0e45
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-05-20 16:17:10 +0200

    [add] errori create completo

--- resources/views/Comics/create.blade.php
+++ resources/views/Comics/create.blade.php
@@ -47,7 +47,13 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Title (*)</label>
-            <input name="title" type="text" class="form-control" id="title" value="{{ old('title') }}">
+            <input name="title" type="text" class="form-control @error('title') is-invalid  @enderror" id="title"
+                value="{{ old('title') }}">
+            @error('title')
+                <small class="text-danger">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
@@ -55,31 +61,73 @@
         </div>
         <div class="mb-3">
             <label for="thumb" class="form-label">Thumb</label>
-            <input name="thumb" type="text" class="form-control" id="thumb" value="{{ old('thumb') }}">
+            <input name="thumb" type="text" class="form-control @error('thumb') is-invalid  @enderror" id="thumb"
+                value="{{ old('thumb') }}">
+            @error('thumb')
+                <small class="text-danger">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Price (*)</label>
-            <input name="price" type="text" class="form-control" id="price" value="{{ old('price') }}">
+            <input name="price" type="text" class="form-control  @error('price') is-invalid  @enderror" id="price"
+                value="{{ old('price') }}">
+            @error('price')
+                <small class="text-danger">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="series" class="form-label">Series</label>
-            <input name="series" type="text" class="form-control" id="series" value="{{ old('series') }}">
+            <input name="series" type="text" class="form-control @error('series') is-invalid  @enderror" id="series"
+                value="{{ old('series') }}">
+            @error('series')
+                <small class="text-danger">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="sale_date" class="form-label">Sale_date (*)</label>
-            <input name="sale_date" type="text" class="form-control" id="sale_date" value="{{ old('sale_date') }}">
+            <input name="sale_date" type="text" class="form-control @error('sale_date') is-invalid  @enderror"
+                id="sale_date" value="{{ old('sale_date') }}">
+            @error('sale_date')
+                <small class="text-danger">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="type" class="form-label">Type (*)</label>
-            <input name="type" type="text" class="form-control" id="type" value="{{ old('type') }}">
+            <input name="type" type="text" class="form-control @error('type') is-invalid  @enderror" id="type"
+                value="{{ old('type') }}">
+            @error('type')
+                <small class="text-danger">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="artists" class="form-label">Artists (*)</label>
-            <input name="artists" type="text" class="form-control" id="artists" value="{{ old('artists') }}">
+            <input name="artists" type="text" class="form-control @error('artists') is-invalid  @enderror" id="artists"
+                value="{{ old('artists') }}">
+            @error('artists')
+                <small class="text-danger">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="writers" class="form-label">Writers (*)</label>
-            <input name="writers" type="text" class="form-control" id="writers" value="{{ old('writers') }}">
+            <input name="writers" type="text" class="form-control @error('writers') is-invalid  @enderror" id="writers"
+                value="{{ old('writers') }}">
+            @error('writers')
+                <small class="text-danger">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
 
         <button class="btn btn-success" type="submit">Invia
